e8c7bc9c569cbfcc82632a68ee13a72e54b127f9
Fickry Iman <fickry.iman@gmail.com>
2023-08-15 20:01:41 +0700

    query and update animals table
    - Add a column species of type string to your animals table
    - Insert more animals data
    - Use database transactions
    - Modify and delete data in SQL
    - Prepare complex queries that answer analytical questions
    - add the screenshot of the results of queries

--- queries.sql
+++ queries.sql
@@ -23,3 +23,32 @@ WHERE name not in ('Gabumon');
 
 SELECT * FROM public.animals
 WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+
+BEGIN TRANSACTION;
+ALTER TABLE public.animals RENAME species TO unspecified;
+ROLLBACK TRANSACTION;
+
+BEGIN TRANSACTION;
+UPDATE public.animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE public.animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT TRANSACTION;
+DELETE FROM public.animals;
+ROLLBACK TRANSACTION;
+
+BEGIN TRANSACTION;
+DELETE FROM public.animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT delete_animals;
+UPDATE public.animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT delete_animals;
+UPDATE public.animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT TRANSACTION;
+
+
+SELECT COUNT(*) FROM public.animals;
+SELECT COUNT(*) FROM public.animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM public.animals;
+SELECT neutered, escape_attempts FROM public.animals ORDER BY escape_attempts DESC LIMIT 1;
+SELECT MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM public.animals;
+SELECT AVG(escape_attempts) FROM public.animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
+
