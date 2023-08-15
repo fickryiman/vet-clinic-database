b761ae492a3fb87e361d983c6fb2e0fef84e9335
Fickry Iman <fickry.iman@gmail.com>
2023-08-15 08:08:05 +0700

    Add the query and the query result screenshots

--- data.sql
+++ data.sql
@@ -1,5 +1,21 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO public.animals (
+id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+'1'::integer, 'Agumon'::text, '2020-02-03'::date, '0'::integer, true::boolean, '10.23'::numeric)
+ returning id;
+
+INSERT INTO public.animals (
+id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+'1'::integer, 'Gabumon'::text, '2018-11-15'::date, '2'::integer, true::boolean, '8'::numeric)
+ returning id;
+
+INSERT INTO public.animals (
+id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+'1'::integer, 'Pikachu'::text, '2021-01-07'::date, '1'::integer, false::boolean, '15.04'::numeric)
+ returning id;
+
+INSERT INTO public.animals (
+id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+'1'::integer, 'Devimon'::text, '2017-05-12'::date, '5'::integer, true::boolean, '11'::numeric)
+ returning id;
--- queries.sql
+++ queries.sql
@@ -1,3 +1,25 @@
 /*Queries that provide answers to the questions from all projects.*/
+SELECT * FROM animals WHERE name = 'Luna';
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM public.animals WHERE name LIKE '%mon';
+
+SELECT * FROM public.animals
+WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
+SELECT * FROM public.animals
+WHERE neutered = 'true' AND escape_attempts < 3;
+
+SELECT name, date_of_birth FROM public.animals
+WHERE name in ('Agumon', 'Pikachu');
+
+SELECT name, escape_attempts FROM public.animals
+WHERE weight_kg > 10.5;
+
+SELECT * FROM public.animals
+WHERE neutered = 'true';
+
+SELECT * FROM public.animals
+WHERE name not in ('Gabumon');
+
+SELECT * FROM public.animals
+WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
