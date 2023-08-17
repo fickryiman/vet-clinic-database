baa48f73ed6e64c65bfcaf11d8bc75aeb8b171b9
Fickry Iman <fickry.iman@gmail.com>
2023-08-17 23:23:02 +0700

    Query multiple tables

--- queries.sql
+++ queries.sql
@@ -55,3 +55,30 @@ SELECT neutered, escape_attempts FROM public.animals ORDER BY escape_attempts DE
 SELECT MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM public.animals GROUP BY species;
 SELECT AVG(escape_attempts) FROM public.animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
 
+
+SELECT A.id, name, date_of_birth, escape_attempts, neutered, weight_kg, species_id, owner_id, full_name
+	FROM public.animals A JOIN public.owners O ON A.owner_id = O.id
+	WHERE O.full_name = 'Melody Pond';
+
+SELECT A.id, A.name, date_of_birth, escape_attempts, neutered, weight_kg, owner_id, species_id, S.name
+	FROM public.animals A JOIN public.species S ON A.species_id = S.id
+	WHERE S.name = 'Pokemon';
+
+SELECT full_name, A.name FROM public.owners O LEFT JOIN public.animals A ON O.id = A.owner_id;
+
+SELECT species_id, COUNT(*) AS animal_count FROM public.animals GROUP BY species_id;
+
+SELECT A.id, A.name, date_of_birth, escape_attempts, neutered, weight_kg, species_id, S.name AS species_name, owner_id, O.full_name AS owner_name
+	FROM public.animals A JOIN public.owners O ON A.owner_id = O.id JOIN public.species S ON A.species_id = S.id
+	WHERE O.full_name = 'Jennifer Onwell' AND S.name = 'Digimon';
+
+SELECT A.id, A.name, date_of_birth, escape_attempts, neutered, weight_kg, species_id, S.name AS species_name, owner_id, O.full_name AS owner_name
+	FROM public.animals A JOIN public.owners O ON A.owner_id = O.id JOIN public.species S ON A.species_id = S.id
+	WHERE O.full_name = 'Dean Winchester' AND A.escape_attempts = 0;
+
+SELECT O.full_name AS owner_name, COUNT(A.owner_id) AS animal_count
+FROM public.animals A
+JOIN public.owners O ON A.owner_id = O.id
+GROUP BY O.full_name
+ORDER BY animal_count DESC
+LIMIT 1;
